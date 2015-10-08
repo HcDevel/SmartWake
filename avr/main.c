aaf94877619ecd443ce78430a2d6913c570bd8de
Robin Meis <robin.meis@smartnoob.de>
2015-10-08 18:45:03 +0200

    Low battery warning

--- avr/main.c
+++ avr/main.c
@@ -13,7 +13,7 @@
 void ADC_Init(void) {
   ADMUX = (1<<REFS1) | (1<<REFS0);
   ADCSRA = (1<<ADPS1) | (1<<ADPS0);
-  ADCSRA |= (1<<ADEN); 
+  ADCSRA |= (1<<ADEN);
   ADCSRA |= (1<<ADSC);
   while (ADCSRA & (1<<ADSC) );
   (void) ADCW;
@@ -34,16 +34,37 @@ uint16_t ADC_Read_Avg( uint8_t channel, uint8_t nsamples ) {
   return (uint16_t)( sum / nsamples );
 }
 
+void battery_init(void) {
+  //TODO: Port Pin for Transistor as output
+  DDRB |= (1<<0); //Battery LOW (red) LED
+}
+
+void battery_check(void) {
+  //TODO: Port Pin for Transistor high
+  if (ADC_Read_Avg(0,2)<=820) {
+    uart_puts("Low battery voltage! Replace soon!\n");
+    PORTB ^= (1<<0);
+  } else {
+    uart_puts("Battery Voltage OK!\n");
+    PORTB &=~ (1<<0);
+  }
+  //TODO: Port Pin for Transistor low
+}
+
 
 int main(void) {
   uart_init(UART_BAUD_SELECT(9600,F_CPU));
   ADC_Init();
   sei();
+  battery_init();
+  uint16_t dms;
   char buffer[10];
   for(;;) {
-    itoa(ADC_Read_Avg(0,2), buffer,10);
+    dms = ADC_Read_Avg(1,2);
+    itoa(dms, buffer,10);
     uart_puts(buffer);
     uart_puts("\n");
+    battery_check();
     _delay_ms(1000);
   }
   return 0;
